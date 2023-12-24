f205c89590b02fd8dd9726bd5e6c97cabaa993a7
Breiddy Garcia <breiddysubzero@gmail.com>
2023-12-24 18:14:11 -0400

    ultima seccion del home agregada

---
+++ MetaMusic/Data/TestData/Track.cs
@@ -0,0 +1,12 @@
+﻿namespace MetaMusic.Data.TestData
+{
+    public class Track
+    {
+
+        public TimeSpan   Duracion{get; set;}
+        public string Titulo { get; set; } = "";
+
+        public int Calificacion;
+        public Album Album { get; set; } = new Album();
+    }
+}
